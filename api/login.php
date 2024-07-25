471a2d22ac00a19f686e57353d40f7a8173b12d1
imvino <49083863+imvino@users.noreply.github.com>
2024-07-25 16:56:11 +0530

    multiview

--- api/login.php
+++ api/login.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-require_once("../../auth/authSystem.php");
+require_once("../auth/authSystem.php");
 
 // Get JSON input
 $data = json_decode(file_get_contents("php://input"));
